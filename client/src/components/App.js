fa8d3907a1bbd1db2c9ebd4378dd8f24f20a3712
Pritam <pritam@test.com>
2019-02-18 19:05:11 +0530

    changes in client

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hi There
+        <a href="/auth/google">Sign in with Google</a>
+      </div>
+    );
+  }
+}
+
+export default App;
