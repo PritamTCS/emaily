794b4a9966ce7e9ffdfd0d369b5ded168088fa8f
Pritam <pritam123@gmail.com>
2019-02-19 10:03:16 +0530

    materialize.css config added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./Header";
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hi There
-        <a href="/auth/google">Sign in with Google</a>
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </Router>
+
+        {/* <a href="/auth/google">Sign in with Google</a> */}
       </div>
     );
   }
