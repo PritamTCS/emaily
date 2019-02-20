4795e96382a9c435181f92013955b784f660be91
Pritam <pritam@test.com>
2019-02-20 17:38:00 +0530

    stripe payments module added

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+console.log("STRIPE KEY :", process.env.REACT_APP_STRIPE_KEY);
+console.log("Environment : ", process.env.NODE_ENV);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
