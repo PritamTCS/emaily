9a7585e6fb5d18caf5dc7eee627ae2e99f46d6a6
Pritam <pritam@test.com>
2019-02-21 19:36:38 +0530

    Credits added

--- index.js
+++ index.js
@@ -31,5 +31,19 @@ app.use(bodyParser.json());
 
 authRoutes(app);
 billingRoutes(app);
+
+if (process.env.NODE_ENV === "production") {
+  // express will serve up prod assets
+  // like main.js or main.css file
+  app.use(express.static("client/build"));
+
+  // express will serve up index.html file
+  // if it does not recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,21 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {
+  app.post("/api/stripe", requireLogin, (req, res) => {
     console.log(req.body);
-    stripe.charges.create({
-      amount: 500,
-      currency: "usd",
-      description: "$5 for 5 credits",
-      source: req.body.id // token
-    });
+    stripe.charges
+      .create({
+        amount: 500,
+        currency: "usd",
+        description: "$5 for 5 credits",
+        source: req.body.id // token
+      })
+      .then(charge => {
+        console.log(charge);
+        req.user.credits += 5;
+        req.user.save().then(user => res.send(user));
+      });
   });
 };
