ab411b93b52481315206e559a8e93e47cb61a3a3
Pritam <pritam@test.com>
2019-03-28 17:19:43 +0530

    /sendgrid_webhook.sh added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 export const fetchUser = () => {
   return dispatch => {
@@ -29,6 +29,17 @@ export const submitSurvey = (formValues, history) => {
       history.push("/surveys");
       dispatch({
         type: FETCH_USER,
+        payload: res.data
+      });
+    });
+  };
+};
+
+export const fetchSurveys = () => {
+  return dispatch => {
+    axios.get("/api/surveys").then(res => {
+      dispatch({
+        type: FETCH_SURVEYS,
         payload: res.data
       });
     });
